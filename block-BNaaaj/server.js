5ce0c21986ecb2c8238d1ae51b0d1887b3e5625e
akk26 <anujbph@gmail.com>
2021-10-13 22:27:57 +0530

    Completed assignment 1

---
+++ block-BNaaaj/server.js
@@ -0,0 +1,14 @@
+var url = require("url");
+
+const parsedUrl = url.parse(
+  `https://airindia.com/fares/calculate?from=delhi&to=detroit`
+  , true);
+
+
+
+console.log(parsedUrl.query, "query");
+
+console.log(parsedUrl.pathname, 'pathname');
+
+console.log(parsedUrl.protocol, "protocol");
+
