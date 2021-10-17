a71940ed1a16e908a73e01dc1c2050f1531bd019
akk26 <anujbph@gmail.com>
2021-10-17 21:58:29 +0530

    Refactor

--- block-BNaaau/server.js
+++ block-BNaaau/server.js
@@ -4,185 +4,185 @@ var fs = require('fs');
 
 var url = require('url');
 
-var server = http.createServer(handleRequest);
+// var server = http.createServer(handleRequest);
 
-function handleRequest(req, res){
-  console.log(req, "request");
-  console.log(res, "response");
-  res.end();
-}
+// function handleRequest(req, res){
+//   console.log(req, "request");
+//   console.log(res, "response");
+//   res.end();
+// }
 
-server.listen(5000, () => {
-  console.log("Listening at 5k port");
-})
-
-
-
-var http2 = require('http');
-
-var server2 = http2.createServer(handleRequest);
-
-function handleRequest(req, res) {
-  res.end("My first server in NodeJS")
-}
-
-server2.listen(5100, () => {
-  console.log("Listening at 5.1k port");
-})
+// server.listen(5000, () => {
+//   console.log("Listening at 5k port");
+// })
 
 
 
-var http3 = require('http');
+// var http2 = require('http');
 
-var server3 = http3.createServer(handleRequest);
+// var server2 = http2.createServer(handleRequest);
 
-function handleRequest(req, res) {
-  console.log(req.headers, "headers");
-  res.end(req.headers['user-agent'])
-}
+// function handleRequest(req, res) {
+//   res.end("My first server in NodeJS")
+// }
 
-server3.listen(5500, () => {
-  console.log("Listening at 5.5k port");
-})
-
-
-
-var http4 = require('http');
-
-var server4 = http4.createServer(handleRequest);
-
-function handleRequest(req, res) {
-  console.log(req.url, 'URL');
-  console.log(req.method, 'Method');
-  let data = 'url= ' + req.url + ', method= ' + req.method;
-  res.end(data);
-}
-
-server4.listen(5566, () => {
-  console.log('Listening at 5566 port');
-});
+// server2.listen(5100, () => {
+//   console.log("Listening at 5.1k port");
+// })
 
 
 
-var http5 = require('http');
+// var http3 = require('http');
 
-var server5 = http5.createServer(handleRequest);
+// var server3 = http3.createServer(handleRequest);
 
-function handleRequest(req, res) {
-  let data = req.headers;
-  res.end(data.toString());
-}
+// function handleRequest(req, res) {
+//   console.log(req.headers, "headers");
+//   res.end(req.headers['user-agent'])
+// }
 
-server5.listen(7000, () => {
-  console.log('Listening at 7k port');
-});
-
-
-
-var http6 = require('http');
-
-var server6 = http6.createServer(handleRequest);
-
-function handleRequest(req, res) {
-  res.statusCode = 202;
-  res.end();
-}
-
-server6.listen(3333, () => {
-  console.log('Listening at 3333 port');
-});
+// server3.listen(5500, () => {
+//   console.log("Listening at 5.5k port");
+// })
 
 
 
-var http7 = require('http');
+// var http4 = require('http');
 
-var server7 = http7.createServer(handleRequest);
+// var server4 = http4.createServer(handleRequest);
 
-function handleRequest(req, res) {
-  res.writeHead(202, { 'Content-Type': 'text/html'});
-  res.end('<h3>Welcome to altcampus</h3>');
-}
+// function handleRequest(req, res) {
+//   console.log(req.url, 'URL');
+//   console.log(req.method, 'Method');
+//   let data = 'url= ' + req.url + ', method= ' + req.method;
+//   res.end(data);
+// }
 
-server7.listen(8000, () => {
-  console.log('Listening at 8000 port');
-});
-
-
-
-var http8 = require('http');
-
-var server8 = http8.createServer(handleRequest);
-
-function handleRequest(req, res) {
-  res.writeHead(202, { 'Content-Type': 'text/json'});
-  res.end(`{success: true, message:Welcome to Nodejs}`);
-}
-
-server8.listen(8888, () => {
-  console.log('Listening at 8888 port');
-});
+// server4.listen(5566, () => {
+//   console.log('Listening at 5566 port');
+// });
 
 
 
-var http9 = require('http');
+// var http5 = require('http');
 
-var server9 = http9.createServer(handleRequest);
+// var server5 = http5.createServer(handleRequest);
 
-function handleRequest(req, res) {
-  if (req.method === 'POST' && req.url === '/index') {
-    res.writeHead(202, { 'Content-Type': 'text/html' });
-    res.end('<h2>posted for first time</h2>');   
-  }
-}
+// function handleRequest(req, res) {
+//   let data = req.headers;
+//   res.end(JSON.stringify(data));
+// }
 
-server9.listen(5050, () => {
-  console.log('Listening at 5050 port');
-});
+// server5.listen(7000, () => {
+//   console.log('Listening at 7k port');
+// });
 
 
 
-var http10 = require('http');
+// var http6 = require('http');
 
-var server10 = http10.createServer(handleRequest);
+// var server6 = http6.createServer(handleRequest);
 
-function handleRequest(req, res) {
-  if (req.method === 'GET' && req.url === '/') {
-    res.writeHead(202, { 'Content-Type': 'text/plain' });
-    res.end('Anuj Kumar');   
-  } else if (req.method === 'GET' && req.url === '/about') {
-    res.writeHead(202, { 'Content-Type': 'text/html' });
-    res.end('<h2>Anuj Kumar</h2>');  
-  } else {
-    res.statusCode = 404;
-    res.end();
-  }
-}
+// function handleRequest(req, res) {
+//   res.statusCode = 202;
+//   res.end();
+// }
 
-server10.listen(2345, () => {
-  console.log('Listening at 2345 port');
-});
+// server6.listen(3333, () => {
+//   console.log('Listening at 3333 port');
+// });
 
 
 
-var http11 = require('http');
+// var http7 = require('http');
 
-var server11 = http11.createServer(handleRequest);
+// var server7 = http7.createServer(handleRequest);
 
-function handleRequest(req, res) {
-  if (req.method === 'GET' && req.url === '/users') {
-    res.writeHead(202, { 'Content-Type': 'text/html' });
-    res.end('<form> <label>Anuj Kumar</label>  <input type="text" placeholder = "Enter your email"></form>');
-  } else if (req.method === 'POST' && req.url === '/users') {
-    res.end('Posted for the second time');
-  } else {
-    res.statusCode = 404;
-    res.end();
-  }
-}
+// function handleRequest(req, res) {
+//   res.writeHead(202, { 'Content-Type': 'text/html'});
+//   res.end('<h3>Welcome to altcampus</h3>');
+// }
 
-server11.listen(2345, () => {
-  console.log('Listening at 2345 port');
-});
+// server7.listen(8000, () => {
+//   console.log('Listening at 8000 port');
+// });
+
+
+
+// var http8 = require('http');
+
+// var server8 = http8.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   res.writeHead(202, { 'Content-Type': 'text/json'});
+//   res.end(JSON.stringify({success: true, message: 'Welcome to Nodejs'}));
+// }
+
+// server8.listen(8888, () => {
+//   console.log('Listening at 8888 port');
+// });
+
+
+
+// var http9 = require('http');
+
+// var server9 = http9.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   if (req.method === 'POST' && req.url === '/index') {
+//     res.writeHead(202, { 'Content-Type': 'text/html' });
+//     res.end('<h2>posted for first time</h2>');   
+//   }
+// }
+
+// server9.listen(5050, () => {
+//   console.log('Listening at 5050 port');
+// });
+
+
+
+// var http10 = require('http');
+
+// var server10 = http10.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   if (req.method === 'GET' && req.url === '/') {
+//     res.writeHead(202, { 'Content-Type': 'text/plain' });
+//     res.end('Anuj Kumar');   
+//   } else if (req.method === 'GET' && req.url === '/about') {
+//     res.writeHead(202, { 'Content-Type': 'text/html' });
+//     res.end('<h2>Anuj Kumar</h2>');  
+//   } else {
+//     res.statusCode = 404;
+//     res.end();
+//   }
+// }
+
+// server10.listen(2345, () => {
+//   console.log('Listening at 2345 port');
+// });
+
+
+
+// var http11 = require('http');
+
+// var server11 = http11.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   if (req.method === 'GET' && req.url === '/users') {
+//     res.writeHead(202, { 'Content-Type': 'text/html' });
+//     res.end('<form> <label>Anuj Kumar</label>  <input type="text" placeholder = "Enter your email"></form>');
+//   } else if (req.method === 'POST' && req.url === '/users') {
+//     res.end('Posted for the second time');
+//   } else {
+//     res.statusCode = 404;
+//     res.end();
+//   }
+// }
+
+// server11.listen(2345, () => {
+//   console.log('Listening at 2345 port');
+// });
 
 //
 
@@ -193,10 +193,11 @@ var http12 = require('http');
 var server12 = http12.createServer(handleRequest);
 
 function handleRequest(req, res) {
-  var parsedUrl = url.parse(req.url);
+  var parsedUrl = url.parse(req.url, true);
   var pathName = parsedUrl.pathName;
   console.log(pathName);
-  res.end();
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(parsedUrl.query));
 }
 
 server12.listen(3000, () => {
